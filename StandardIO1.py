0bb69959e7c2b96d61f361ed164cd9a796a66330
Satish Kumar Gollapudi <SatishKumarGollapudi@users.noreply.github.com>
2024-03-08 12:55:43 +0530

    StandardIO1.py

---
+++ StandardIO1.py
@@ -0,0 +1,22 @@
+Given 3 numbers, you are asked to find sum of those 3 numbers.
+
+Input Format
+
+First 3 lines consists of 3 numbers, one per line
+
+Constraints
+
+Each number is between 1 to 1000.
+
+Output Format
+
+Single line consisting of a number - which is sum of 3 numbers.
+
+Sample Input 0
+
+10
+20
+30
+Sample Output 0
+
+60
